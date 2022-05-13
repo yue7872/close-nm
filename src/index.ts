2884f57c5dff940d9d3799f0a79b2e6f16bf0ac4
yue7872 <2258462872@qq.com>
2022-05-13 13:33:52 +0800

    feat: 新增命令显示位置 。

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { window } from 'vscode'
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import type { ExtensionContext } from 'vscode'
+import { commands, window } from 'vscode'
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
 
-export function activate() {
-  window.showInformationMessage('Hello')
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = commands.registerCommand('close.node_modules', () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    window.showInformationMessage('Hello World!')
+  })
+
+  context.subscriptions.push(disposable)
 }
 
-export function deactivate() {
-
-}
+// this method is called when your extension is deactivated
+export function deactivate() {}
